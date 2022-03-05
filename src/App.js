aa3408ab8addd1ac061d7c206da03fc7c189bfad
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-05 14:16:10 +0600

    📎initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import styled from "styled-components";
+
+export default function App() {
+  return <Container></Container>;
+}
+
+const Container = styled.div``;
+
+// export default App;
